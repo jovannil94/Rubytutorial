b8ee10b05718eaa36286dd7f56da8a3f1e86d1f6
Jovanni Luna <jluna@foursquare.com>
2021-09-30 15:42:51 -0400

    creates Model Category, sets a migration for joined table of articles and categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_30_193651) do
+ActiveRecord::Schema.define(version: 2021_09_30_194153) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,17 @@ ActiveRecord::Schema.define(version: 2021_09_30_193651) do
     t.string "location"
     t.integer "user_id", null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "articles_categories", id: false, force: :cascade do |t|
+    t.integer "article_id", null: false
+    t.integer "category_id", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "profiles", force: :cascade do |t|
